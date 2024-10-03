c56cc2baf30d0a93b1fa184c4c62f27c95c48f93
Krishna Bharadwaj <krishnabharadwaj1995@gmail.com>
2024-10-03 10:07:28 +0530

    transactions enabled pool connection

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,9 +1,8 @@
 import { config } from "dotenv";
-import { neon } from "@neondatabase/serverless";
-import { drizzle } from "drizzle-orm/neon-http";
+import { Pool } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-serverless";
 
 config({ path: ".env" }); // or .env.local
 
-const sql = neon(process.env.DATABASE_URL!);
-
-export const db = drizzle(sql);
+const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+export const db = drizzle(pool);
