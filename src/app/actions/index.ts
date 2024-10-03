3968f6fc815ac100488a59f8aa1ddb56ab8bf6bc
Krishna Bharadwaj <krishnabharadwaj1995@gmail.com>
2024-10-03 09:50:39 +0530

    transactions

--- src/app/actions/index.ts
+++ src/app/actions/index.ts
@@ -26,46 +26,53 @@ export async function createForm(input: FormInputType) {
     throw new Error("Unauthorized");
   }
 
-  try {
-    // Create the form
-    const newForm: NewFormType = {
-      ...input,
-      userId,
-    };
-    const [createdForm] = await db.insert(forms).values(newForm).returning();
+  return await db
+    .transaction(async (tx) => {
+      try {
+        // Create the form
+        const newForm: NewFormType = {
+          ...input,
+          userId,
+        };
+        const [createdForm] = await tx
+          .insert(forms)
+          .values(newForm)
+          .returning();
 
-    // Create questions and their options
-    for (let i = 0; i < input.questions.length; i++) {
-      const q = input.questions[i];
-      const newQuestion: NewQuestionType = {
-        ...q,
-        formId: createdForm.id,
-        order: i,
-      };
-      const [createdQuestion] = await db
-        .insert(questions)
-        .values(newQuestion)
-        .returning();
+        // Create questions and their options
+        for (let i = 0; i < input.questions.length; i++) {
+          const q = input.questions[i];
+          const newQuestion: NewQuestionType = {
+            ...q,
+            formId: createdForm.id,
+            order: i,
+          };
+          const [createdQuestion] = await tx
+            .insert(questions)
+            .values(newQuestion)
+            .returning();
 
-      // If the question has options, create them
-      if (q.options && q.options.length > 0) {
-        const newOptions: NewQuestionOptionType[] = q.options.map(
-          (option, index) => ({
-            ...option,
-            questionId: createdQuestion.id,
-            order: index,
-          })
-        );
-        await db.insert(questionOptions).values(newOptions);
+          // If the question has options, create them
+          if (q.options && q.options.length > 0) {
+            const newOptions: NewQuestionOptionType[] = q.options.map(
+              (option, index) => ({
+                ...option,
+                questionId: createdQuestion.id,
+                order: index,
+              })
+            );
+            await tx.insert(questionOptions).values(newOptions);
+          }
+        }
+
+        return createdForm;
+      } catch (error) {
+        console.error("Failed to create form:", error);
+        throw new Error("Failed to create form");
       }
-    }
-
-    return createdForm;
-  } catch (error) {
-    console.error("Failed to create form:", error);
-    throw new Error("Failed to create form");
-  } finally {
-    revalidatePath("/forms");
-    revalidatePath("/dashboard");
-  }
+    })
+    .finally(() => {
+      revalidatePath("/forms");
+      revalidatePath("/dashboard");
+    });
 }
