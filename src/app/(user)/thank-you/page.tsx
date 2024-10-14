57f5e31a12ff724c161428b0fc2eba62c16990a1
Krishna Bharadwaj <krishnabharadwaj1995@gmail.com>
2024-10-14 17:41:57 +0530

    final

--- src/app/(user)/thank-you/page.tsx
+++ src/app/(user)/thank-you/page.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const ThankYou = () => {
   return (
-    <div className="flex h-screen items-center justify-center text-lg text-purple-400">
-      Thank you for responding! We&apos;ve received your responses
+    <div className="flex h-screen items-center justify-center text-lg max-w-xl text-purple-400">
+      Thank you for responding! We&apos;ve received your responses.
     </div>
   );
 };
