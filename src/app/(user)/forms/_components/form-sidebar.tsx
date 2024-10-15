18c76acc38a4045efcd9eb9d2e46a63359b032fb
Krishna Bharadwaj <krishnabharadwaj1995@gmail.com>
2024-10-15 10:45:44 +0530

    color add

--- src/app/(user)/forms/_components/form-sidebar.tsx
+++ src/app/(user)/forms/_components/form-sidebar.tsx
@@ -1,10 +1,5 @@
 "use client";
 
-import { useFormStore } from "@/app/store";
-import { Button } from "@/components/ui/button";
-import { Label } from "@/components/ui/label";
-import { Textarea } from "@/components/ui/textarea";
-import { SparklesIcon } from "lucide-react";
 import React, { useState } from "react";
 import AiInput from "./ai-input";
 import ColorInput from "./color-input";
